0910f42967b42486bfdd62a3b6947b5bd1d9a23f
Nikhil Lilhare <nikhillilhare90@gmail.com>
2026-07-03 16:44:59 +0530

    creating menu with 2 switch option in main file

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,4 +1,49 @@
 package main;
 
+import service.serviceConsole.CustomerService;
+
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CustomerService customerService = new CustomerService();
+        int choice;
+        do{
+            System.out.println("\n========================================");
+            System.out.println(" ONLINE FOOD ORDERING SYSTEM ");
+            System.out.println("========================================");
+            System.out.println("1. Register User");
+            System.out.println("0. Exit");
+            System.out.print("Enter Choice : ");
+            choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice){
+                case 0:
+                    System.out.print("Thank You");
+                    break;
+                //Register New Customer using arraylist and used property add()
+                case 1:
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter Email: ");
+                    String email = sc.nextLine();
+                    System.out.print("Enter Mobile: ");
+                    String mobile = sc.nextLine();
+                    System.out.print("Enter Password: ");
+                    String password = sc.nextLine();
+                    boolean registered = customerService.registerCustomer(name,email,mobile,password);
+                    if(registered){
+                        System.out.print("Customer Registered Successfully. ");
+                    }else {
+                        System.out.println("Email Already Exists and failed to add Same Registration");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+
+        }while (choice !=0);
+    }
 }
